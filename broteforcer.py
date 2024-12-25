6341042b4b01c1ec109f62708d38a5bfcef57ccf
cy6erlem0n <dmytriy.drozd.2014@gmail.com>
2024-12-25 17:28:31 +0100

    creating broteforcer, adding available options, starting threads for bruteforce

--- broteforcer.py
+++ broteforcer.py
@@ -11,9 +11,34 @@ def banner ():
     print(f"#########################")
 
 def start(argv):
+    banner()
+    try:
+        opts, args = getopt.getopt(argv, "u:w:f:t")
+    except getopt.GetoptError:
+        print(f"Error on arguements")
+        sys.exit()
+    for opt,arg in opts:
+        if opt == '-u':
+            user = arg
+        elif opt == '-w':
+            url = arg
+        elif opt == '-f':
+            passlist = arg
+        elif opt == '-t':
+            threads = arg
+    try:
+        f = open(passlist, "r")
+        passwords = f.readlines()
+    except:
+        print(f"[!!]Cant open that file")
+        sys.exit()
+    launcher_thread(passwords, threads, user, url):
+    
+
+
 
 if __name__ == "__main__":
     try:
         start(sys.argv[1:])
     except KeyboardInterrupt:
-        print(f"Interrupted!!!")
+        print(f"[!!]Interrupted")
